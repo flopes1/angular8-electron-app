f1ab3e765d8a5ad44542c8292ca777b983260631
Filipe Lopes <filipelopes15@gmail.com>
2019-06-15 14:06:54 -0300

    Configure electron reload

--- electron/main.ts
+++ electron/main.ts
@@ -4,6 +4,8 @@ import * as url from 'url';
 
 let window: BrowserWindow;
 const production = false;
+const args = process.argv.slice(1);
+const serve = args.some(val => val === '--serve');
 
 function createWindow() {
 
@@ -20,17 +22,25 @@ function createWindow() {
         },
     });
 
-    window.loadURL(
-        url.format({
-            pathname: path.join(__dirname, `/../../dist/angular8-electron-app/index.html`),
-            protocol: 'file:',
-            slashes: true,
-        })
-    );
+    if (serve) {
+        require('electron-reload')(__dirname, {
+            electron: require(`${__dirname}/../../node_modules/electron`)
+        });
+        window.loadURL('http://localhost:4200');
 
+    } else {
+
+        window.loadURL(
+            url.format({
+                pathname: path.join(__dirname, `/../../dist/angular8-electron-app/index.html`),
+                protocol: 'file:',
+                slashes: true,
+            })
+        );
+    }
     window.removeMenu();
 
-    if (!production) {
+    if (serve) {
         window.webContents.openDevTools();
     }
 }
